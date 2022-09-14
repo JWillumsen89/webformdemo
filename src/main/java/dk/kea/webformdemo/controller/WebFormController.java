a245be07689d9c3fa81bf421cc60336d0ccc3947
JWillumsen89 <100369030+JWillumsen89@users.noreply.github.com>
2022-09-14 11:03:48 +0200

    Forms are working

--- src/main/java/dk/kea/webformdemo/controller/WebFormController.java
+++ src/main/java/dk/kea/webformdemo/controller/WebFormController.java
@@ -1,8 +1,11 @@
 package dk.kea.webformdemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class WebFormController {
@@ -10,6 +13,25 @@ public class WebFormController {
   @GetMapping("/")
   public String showForm() {
     return "webform";
+  }
+
+  @PostMapping("/creategreeting")
+  public String showGreeting(@RequestParam("name") String name, @RequestParam("age") int age, RedirectAttributes redirectAttributes) {
+    System.out.println(name + age); // Replace wih database create/update/delete
+
+    redirectAttributes.addAttribute("name", name);
+    redirectAttributes.addAttribute("age", age);
+
+    return "redirect:/creategreeting";
+  }
+
+  @GetMapping("/creategreeting")
+  public String createGreetingSite(@RequestParam("name") String name, @RequestParam("age") int age, Model model) {
+
+    model.addAttribute("name", name);
+    model.addAttribute("age", age);
+
+    return "greetingsite";
   }
 
 }
